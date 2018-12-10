6055b5cb2b55c4e382f3d2d1476bd2e5d897d8d5
Maciej Malkowski <maciej.malkowski@agoda.com>
2018-12-10 13:01:56 +0700

    Updates

--- precache-manifest.4a6fb3cc46bebb4da9e11b8c0bd697e4.js
+++ precache-manifest.4a6fb3cc46bebb4da9e11b8c0bd697e4.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/css-in-js-stress/static/js/runtime~main.c0a31c96.js"
   },
   {
-    "revision": "0d1dbaa668afe25cf691",
-    "url": "/css-in-js-stress/static/js/main.0d1dbaa6.chunk.js"
+    "revision": "1a1c5abe6e1f37985e4a",
+    "url": "/css-in-js-stress/static/js/main.1a1c5abe.chunk.js"
   },
   {
     "revision": "7fcf27dbb98ea1c41eb3",
     "url": "/css-in-js-stress/static/js/1.7fcf27db.chunk.js"
   },
   {
-    "revision": "0d1dbaa668afe25cf691",
+    "revision": "1a1c5abe6e1f37985e4a",
     "url": "/css-in-js-stress/static/css/main.db60ad7b.chunk.css"
   },
   {
-    "revision": "3bd00d009ec06dd9e5989fe3263ea30a",
+    "revision": "cd5f80547bbc23caa5f6003e47185d54",
     "url": "/css-in-js-stress/index.html"
   }
 ];
